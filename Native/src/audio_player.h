69e29908654e858b25c4f1d7ab823fae15471863
j4587698 <j4587698@users.noreply.github.com>
2026-06-11 16:25:53 +0800

    fix: propagate decoder stream failures

--- Native/src/audio_player.h
+++ Native/src/audio_player.h
@@ -11,6 +11,7 @@ extern "C" {
 // 不透明句柄类型
 typedef struct AudioContext AudioContext;
 typedef void (*AudioStopCallback)(void);
+typedef ma_result (*AudioLengthCallback)(void* userdata, int64_t* length);
 
 #if defined(_WIN32) || defined(__CYGWIN__)
 #  if defined(AUDIO_PLAYER_BUILD)
@@ -27,7 +28,7 @@ typedef void (*AudioStopCallback)(void);
 // 导出函数
 AUDIO_PLAYER_API AudioContext* audio_context_create(void);
 AUDIO_PLAYER_API ma_result audio_init_device(AudioContext* ctx, AudioStopCallback managedCallback, ma_device_notification_proc notification, const ma_format format, const ma_uint32 channels, const ma_uint32 sampleRate);
-AUDIO_PLAYER_API ma_result audio_init_decoder(AudioContext* ctx, ma_decoder_read_proc onRead, ma_decoder_seek_proc onSeek, ma_decoder_tell_proc onTell, void* userdata);
+AUDIO_PLAYER_API ma_result audio_init_decoder(AudioContext* ctx, ma_decoder_read_proc onRead, ma_decoder_seek_proc onSeek, ma_decoder_tell_proc onTell, AudioLengthCallback onGetLength, ma_bool32 canSeek, void* userdata);
 AUDIO_PLAYER_API ma_result audio_play(AudioContext* ctx);
 AUDIO_PLAYER_API ma_result audio_stop(AudioContext* ctx);
 AUDIO_PLAYER_API void audio_cleanup(AudioContext* ctx);
@@ -40,6 +41,7 @@ AUDIO_PLAYER_API ma_result get_duration(AudioContext* ctx, double* duration);
 AUDIO_PLAYER_API float get_volume(AudioContext* ctx);
 AUDIO_PLAYER_API ma_result set_volume(AudioContext* ctx, float volume);
 AUDIO_PLAYER_API ma_device_state get_play_state(AudioContext* ctx);
+AUDIO_PLAYER_API ma_result get_decode_result(AudioContext* ctx);
 
 #ifdef __cplusplus
 }
